3be2b22445c10d8b1ebee751812a4307a241658f
Tania Pan <tania.pan1511@gmail.com>
2024-05-08 20:08:20 +1200

    added print messages for when player inputs

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -18,20 +18,23 @@ public class Game {
     MessageCli.START_ROUND.printMessage(Integer.toString(round));
 
     // getting a valid human input
-
     int playerFingies = -1;
 
     while (playerFingies == -1) {
+
       MessageCli.ASK_INPUT.printMessage();
       String playerInput = Utils.scanner.nextLine();
       int inputInt = Integer.parseInt(playerInput);
+
       if (Utils.isInteger(playerInput)) {
         if ((inputInt <= 5) && (inputInt >= 0)) {
           playerFingies = inputInt;
           break;
         }
       }
+      MessageCli.INVALID_INPUT.printMessage();
     }
+    MessageCli.PRINT_INFO_HAND.printMessage(playerName, Integer.toString(playerFingies));
   }
 
   public void endGame() {}
