b4f695478ee3a2e7b9efaed0fca196f3ab35da03
Tania Pan <tania.pan1511@gmail.com>
2024-05-08 20:16:46 +1200

    fixed to print correct round number

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,15 +6,16 @@ import nz.ac.auckland.se281.Main.Difficulty;
 public class Game {
 
   String playerName;
+  int round;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     playerName = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(playerName);
+    round = 1;
   }
 
   public void play() {
-    int round = 1;
     MessageCli.START_ROUND.printMessage(Integer.toString(round));
 
     // getting a valid human input
@@ -29,12 +30,13 @@ public class Game {
       if (Utils.isInteger(playerInput)) {
         if ((inputInt <= 5) && (inputInt >= 0)) {
           playerFingies = inputInt;
+          MessageCli.PRINT_INFO_HAND.printMessage(playerName, Integer.toString(playerFingies));
           break;
         }
       }
       MessageCli.INVALID_INPUT.printMessage();
     }
-    MessageCli.PRINT_INFO_HAND.printMessage(playerName, Integer.toString(playerFingies));
+    round++;
   }
 
   public void endGame() {}
