fcc20c57b652a500d618a4d6c1e3f7bc604c44d9
Tania Pan <tania.pan1511@gmail.com>
2024-05-08 20:05:06 +1200

    implemented code to take a valid player input

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -3,12 +3,14 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.Choice;
 import nz.ac.auckland.se281.Main.Difficulty;
 
-/** This class represents the Game is the main entry point. */
 public class Game {
+
+  String playerName;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
-    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    playerName = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(playerName);
   }
 
   public void play() {
@@ -16,8 +18,20 @@ public class Game {
     MessageCli.START_ROUND.printMessage(Integer.toString(round));
 
     // getting a valid human input
-    String humanInput = Utils.scanner.nextLine();
 
+    int playerFingies = -1;
+
+    while (playerFingies == -1) {
+      MessageCli.ASK_INPUT.printMessage();
+      String playerInput = Utils.scanner.nextLine();
+      int inputInt = Integer.parseInt(playerInput);
+      if (Utils.isInteger(playerInput)) {
+        if ((inputInt <= 5) && (inputInt >= 0)) {
+          playerFingies = inputInt;
+          break;
+        }
+      }
+    }
   }
 
   public void endGame() {}
