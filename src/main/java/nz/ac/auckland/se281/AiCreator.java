95ee2c430eb2b246189b223089e4741bae752c9e
Tania Pan <tania.pan1511@gmail.com>
2024-05-12 18:53:23 +1200

    improved method of getting a player input at start of new round

---
+++ src/main/java/nz/ac/auckland/se281/AiCreator.java
@@ -0,0 +1,20 @@
+package nz.ac.auckland.se281;
+
+import nz.ac.auckland.se281.Main.Difficulty;
+
+// This class is the factory for Hal-9000
+public class AiCreator {
+
+  public static Ai createAi(Difficulty difficulty) {
+    switch (difficulty) {
+      case EASY:
+        return new Easy();
+        //   case MEDIUM:
+        //     return new Medium();
+        //   case HARD:
+        //     return new Hard();
+      default:
+        return null;
+    }
+  }
+}
--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,36 +6,45 @@ import nz.ac.auckland.se281.Main.Difficulty;
 public class Game {
 
   String playerName;
+  Difficulty difficulty;
+  Choice choice;
   int round;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
-    // the first element of options[0]; is the name of the player
+
     playerName = options[0];
-    MessageCli.WELCOME_PLAYER.printMessage(playerName);
+    this.difficulty = difficulty;
+    this.choice = choice;
     round = 1;
+
+    MessageCli.WELCOME_PLAYER.printMessage(playerName);
   }
 
   public void play() {
     MessageCli.START_ROUND.printMessage(Integer.toString(round));
 
     // getting a valid human input
-    int playerFingies = -1;
 
-    while (playerFingies == -1) {
+    int inputInt;
+    String playerInput;
+    boolean firstInput = true;
 
-      MessageCli.ASK_INPUT.printMessage();
-      String playerInput = Utils.scanner.nextLine();
-      int inputInt = Integer.parseInt(playerInput);
+    do {
 
-      if (Utils.isInteger(playerInput)) {
-        if ((inputInt <= 5) && (inputInt >= 0)) {
-          playerFingies = inputInt;
-          MessageCli.PRINT_INFO_HAND.printMessage(playerName, Integer.toString(playerFingies));
-          break;
-        }
+      // print correct message based on whether it's the first input or not
+      if (firstInput == true) {
+        MessageCli.ASK_INPUT.printMessage();
+        firstInput = false;
+      } else {
+        MessageCli.INVALID_INPUT.printMessage();
       }
-      MessageCli.INVALID_INPUT.printMessage();
-    }
+
+      playerInput = Utils.scanner.nextLine();
+      inputInt = Integer.parseInt(playerInput);
+
+    } while (!Utils.isInteger(playerInput) || !(inputInt <= 5) || !(inputInt >= 0));
+
+    MessageCli.PRINT_INFO_HAND.printMessage(playerName, playerInput);
     round++;
   }
 
